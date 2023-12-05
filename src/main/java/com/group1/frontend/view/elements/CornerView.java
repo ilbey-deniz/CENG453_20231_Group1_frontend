cf4ebd86203560c8956c157eb925aa6df12ae151
Fatih <m.fatih.berberoglu@gmail.com>
2023-12-05 15:11:03 +0300

    Corner click functionality added

--- src/main/java/com/group1/frontend/view/elements/CornerView.java
+++ src/main/java/com/group1/frontend/view/elements/CornerView.java
@@ -1,21 +1,47 @@
 package com.group1.frontend.view.elements;
 
 import com.group1.frontend.components.Corner;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 import static com.group1.frontend.constants.BoardConstants.*;
 
 public class CornerView extends Circle {
-    private double boardXCoordinate;
-    private double boardYCoordinate;
+//    private double boardXCoordinate;
+//    private double boardYCoordinate;
+    Corner corner;
 
 public CornerView(Corner corner) {
-    super(corner.getXCoordinate() * TILE_HEIGHT*V2/4 * 1.10 + xStartOffset, corner.getYCoordinate() * TILE_HEIGHT*1/4 * 1.10 + yStartOffset, 10);
-    double xCoordinate = corner.getXCoordinate() * TILE_HEIGHT*V2/2 * 1.10 + xStartOffset;
-    double yCoordinate = corner.getYCoordinate() * TILE_HEIGHT*3/4 * 1.10 + yStartOffset;
-    this.boardXCoordinate = xCoordinate;
-    this.boardYCoordinate = yCoordinate;
-    setOnMouseClicked(e -> System.out.println("Clicked: " + this));
-    }
+    super(corner.getXCoordinate() * TILE_HEIGHT*V2/4 * 1.10 + xStartOffset,
+            corner.getYCoordinate() * TILE_HEIGHT*1/4 * 1.10 + yStartOffset, TILE_HEIGHT/8
+    );
+    setFill(Color.TRANSPARENT);
+    this.corner = corner;
 
+
+//    setFill(corner.getPlayer().getColor());
+//    double xCoordinate = corner.getXCoordinate() * TILE_HEIGHT*V2/4 * 1.10 + xStartOffset;
+//    double yCoordinate = corner.getYCoordinate() * TILE_HEIGHT*1/4 * 1.10 + yStartOffset;
+//    this.boardXCoordinate = xCoordinate;
+//    this.boardYCoordinate = yCoordinate;
+    setOnMouseClicked(e -> {
+        try {
+            this.occupyCorner();
+        } catch (FileNotFoundException ex) {
+            throw new RuntimeException(ex);
+        }
+    });
 }
+    public void occupyCorner() throws FileNotFoundException {
+        Image image = new Image(new FileInputStream("src/main/resources/assets/house_blue.png"));
+        setFill(new ImagePattern(image));
+        corner.setIsOccupied(true);
+    }
+}
+
+
