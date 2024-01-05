7632f45e3626ebe9e340c46b14dbb090323cdf91
e2448314 <e2448314@ceng.metu.edu.tr>
2024-01-05 16:54:40 +0300

    initial test

---
+++ src/test/java/com/group1/frontend/MainApplicationWrapperTest.java
@@ -0,0 +1,15 @@
+package com.group1.frontend;
+
+import org.junit.jupiter.api.Test;
+import org.testfx.framework.junit5.ApplicationTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainApplicationWrapperTest extends ApplicationTest {
+
+        @Test
+        void main() {
+
+        }
+
+}
